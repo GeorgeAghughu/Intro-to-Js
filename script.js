ce843810d96bca32e7992f61c8de1c3f84b43508
George Aghughu <georgeighodaro@yahoo.com>
2023-07-12 01:14:55 +0200

    intro-to-Js fundamental code added

---
+++ script.js
@@ -0,0 +1,38 @@
+//variable declaration
+//Create a variable called average that computes for the average from grade1 to grade4
+//Arithmetic
+
+let grade1 = 53;
+let grade2 = 96;
+let grade3 = 65;
+let grade4 = 74;
+console.log( grade1 + grade2 + grade3 + grade4);
+
+let passingGrade = 80;
+
+//The avaerage Grade
+let totalGrade = 288;
+console.log(totalGrade/4);
+
+let studentFirstName = "Brandon";
+console.log (studentFirstName);
+let studentLastName = "Newman";
+console.log (studentLastName);
+
+let fName ="Brandon";
+let lName ="Newman";
+let fullName = fName + "" + lName;
+console.log(fullName);
+
+
+let obj = {
+  fullName: "BrandonNewman",
+  AverageGrade: 72,
+  passingGrade : 80,
+}
+console.log (passingGrade);
+
+//Did the student pass?
+let boolf= false;
+console.log (boolf);
+
